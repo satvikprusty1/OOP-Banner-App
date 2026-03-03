87fd603b5625efd4dda772e00628ae63a62b1f1c
satvikprusty1 <amlatel191@gmail.com>
2026-03-03 15:18:37 +0530

    Use case 2

---
+++ PrintOops.java
@@ -0,0 +1,14 @@
+public class PrintOops {
+    
+    public static void main(String[] args) {
+        /*uc2 OOps*/
+        System.out.println(" *****    *****    ******    ***** ");
+        System.out.println("*     *  *     *   *     *  *     *");
+        System.out.println("*     *  *     *   *     *  *      ");
+        System.out.println("*     *  *     *   ******    ***** ");
+        System.out.println("*     *  *     *   *              *");
+        System.out.println("*     *  *     *   *              *");
+        System.out.println(" *****    *****    *         ***** ");
+        
+    }
+}
